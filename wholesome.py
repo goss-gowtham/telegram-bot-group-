3f9b1f6bc6ab50f483425706f08ba27cc78909f4
krsh-37 <36710708+krsh-37@users.noreply.github.com>
2020-05-08 22:02:45 +0530

    added fb page

--- wholesome.py
+++ wholesome.py
@@ -6,7 +6,8 @@ def pre_proc():
     url=['https://www.facebook.com/Strannger11/posts',
          'https://www.facebook.com/pages/category/Art/H-o-r-n-y-6-9-105290237679968/posts',
          "https://www.facebook.com/C-u-t-e-G-i-r-l-s-111939930162073/posts/?ref=page_internal",
-         'https://www.facebook.com/assesandtiddies/posts/?ref=page_internal'
+         'https://www.facebook.com/assesandtiddies/posts/?ref=page_internal',
+         'https://www.facebook.com/sadwatr/posts/?ref=page_internal'
          ]
     #url = random.choice(url)
     x=0
